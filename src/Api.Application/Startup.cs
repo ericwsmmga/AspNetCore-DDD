888bbca0ee03e90f2dca4c03c2bd50b0e21d3466
Eric William da Silva Marques <eric.marques@benner.com.br>
2021-06-01 21:00:17 -0300

    01-06 || Autenticação da API com JWT
    Finalizando a Autenticação da API com JWT

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.CrossCutting.DependencyInjection;
+using Api.Domain.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace application
@@ -29,6 +33,36 @@ namespace application
             ConfigureService.ConfigureDependenciesServices(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
+            var signingConfigurations = new SigningConfigurations();
+            services.AddSingleton(signingConfigurations);
+
+            var tokenConfigurations = new TokenConfigurations();
+            new ConfigureFromConfigurationOptions<TokenConfigurations>(
+                Configuration.GetSection("TokenConfigurations")).Configure(tokenConfigurations);
+            services.AddSingleton(tokenConfigurations);
+
+            services.AddAuthentication(Options =>
+           {
+               Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+               Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+           }).AddJwtBearer(bearerOptions =>
+           {
+               var paramsValidation = bearerOptions.TokenValidationParameters;
+               paramsValidation.IssuerSigningKey = signingConfigurations.Key;
+               paramsValidation.ValidAudience = tokenConfigurations.Audience;
+               paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
+               paramsValidation.ValidateIssuerSigningKey = true;
+               paramsValidation.ValidateLifetime = true;
+               paramsValidation.ClockSkew = TimeSpan.Zero;
+           });
+
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser().Build());
+            });
+
 
             services.AddControllers();
             services.AddSwaggerGen(config =>
@@ -38,8 +72,30 @@ namespace application
                        Title = "Curso de API com AspNerCore 3.1",
                        Version = "v1"
                    });
+
+                   config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                   {
+                       Description = "Entre com o Token JWT",
+                       Name = "Authorization",
+                       In = ParameterLocation.Header,
+                       Type = SecuritySchemeType.ApiKey
+                   });
+
+                   config.AddSecurityRequirement(new OpenApiSecurityRequirement{
+                   {
+                        new OpenApiSecurityScheme{
+                            Reference = new OpenApiReference{
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },new List<string>()
+                   }
+                   });
                }
             );
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
